c79351a36a2790252d63dc0a5e2fe449e15d10d5
Константин Бутузов <k.butuzov@kazanexpress.tech>
2024-04-10 16:01:28 +0300

    remove comments

--- src/main/java/org/example/BubbleSorter.java
+++ src/main/java/org/example/BubbleSorter.java
@@ -2,9 +2,7 @@ package org.example;
 
 public class BubbleSorter {
     public static void sort(int[] array) {
-        // i - номер прохода
         for (int i = 0; i < array.length - 1; i++) {
-            // внутренний цикл прохода
             for (int j = array.length - 1; j > i; j--) {
                 if (array[j - 1] > array[j]) {
                     int tmp = array[j - 1];
